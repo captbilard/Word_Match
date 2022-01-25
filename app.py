d5120411553517ecacca9478f3c7580eb8ad5d2b
Adebayo Osinulu <bilardosinulu@gmail.com>
2022-01-26 00:37:32 +0100

    Made app a Command Line utility with Argparse

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import argparse
 import difflib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_1", help="first text file")
+parser.add_argument("file_2", help="second text file")
+args = parser.parse_args()
 
 matched_word = []
 match_counter = 0
 
-with open("text1.txt", 'r') as f1:
+file_1, file_2 = args.file_1, args.file_2
+
+with open(file_1, 'r') as f1:
     read_file1 = f1.read().split()
 
-with open("text2.txt", 'r') as f2:
+with open(file_2, 'r') as f2:
     read_file2 = f2.read().split()
 
 # for char_a, char_b in zip(read_file1, read_file2):
@@ -17,7 +25,7 @@ with open("text2.txt", 'r') as f2:
 # print(match_counter)
 # print(matched_word)
 
-diff = difflib.HtmlDiff().make_file(read_file1,read_file2, "text1.txt","text2.txt")
+diff = difflib.HtmlDiff().make_file(read_file1,read_file2, file_1, file_2)
 path = "result3.html"
 f = open(path, 'w')
 f.write(diff)
